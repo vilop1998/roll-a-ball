eb59d978078f6ecfbf42154f425e2075311d4f24
vilop1998 <vilopdraw@gmail.com>
2019-10-23 11:03:44 +0200

    Initial commit

---
+++ Assets/Scripts/Movement/MainCharacter/PlayerControlerBasicMovement01.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerControlerBasicMovement01 : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody mainRigyBody;
+    private void FixedUpdate()
+    {
+        float datoHorizontal, datoVertical;
+        Vector3 movement;
+        //Vector2 ejexz;
+        datoHorizontal = Input.GetAxisRaw("Horizontal");
+        datoVertical = Input.GetAxisRaw("Vertical");
+        //ejexz = new Vector2(datoHorizontal, datoVertical);
+        movement = new Vector3(datoHorizontal*speed, mainRigyBody.velocity.y, datoVertical*speed);
+        mainRigyBody.velocity = movement;
+        //mainRigyBody.velocity = ejexz * speed;
+        
+    }    
+}
