75885a1ee493c7c736cce51d5b9a12900b59907a
vilop1998 <vilopdraw@gmail.com>
2019-10-30 11:54:47 +0100

    Everything
    ChangeMain,AddPickUpObject,AddWallsPhisicalsMaterials

--- Assets/Scripts/Movement/MainCharacter/PlayerControlerBasicMovement01.cs
+++ Assets/Scripts/Movement/MainCharacter/PlayerControlerBasicMovement01.cs
@@ -4,17 +4,36 @@ public class PlayerControlerBasicMovement01 : MonoBehaviour
 {
     public float speed;
     public Rigidbody mainRigyBody;
+    private int score;
+    
     private void FixedUpdate()
     {
         float datoHorizontal, datoVertical;
         Vector3 movement;
-        //Vector2 ejexz;
         datoHorizontal = Input.GetAxisRaw("Horizontal");
         datoVertical = Input.GetAxisRaw("Vertical");
-        //ejexz = new Vector2(datoHorizontal, datoVertical);
         movement = new Vector3(datoHorizontal*speed, mainRigyBody.velocity.y, datoVertical*speed);
-        mainRigyBody.velocity = movement;
-        //mainRigyBody.velocity = ejexz * speed;
-        
-    }    
+        mainRigyBody.velocity = movement;                
+    }
+    //Estamos comprobando el contacto entre collaider y onTriggers, no hemos especificado ninguno
+    private void OnTriggerEnter(Collider other)
+    {  
+        //Comparo el tag del objeto contra el que he colisionado, para desactivarlo solo en el caso de que sea un pickup
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            // Si es un PickUp, lo desactivo.
+            other.gameObject.SetActive(false);
+            //score++;
+            score += other.gameObject.GetComponent<RotationCube>().valuePoint;
+            Debug.Log(score);
+        }
+    }
+    /*private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            mainRigyBody.AddForce(new Vector3(10,0,10), ForceMode.Impulse);
+            Debug.Log(mainRigyBody);
+        }
+    }*/
 }
---
+++ Assets/Scripts/Movement/MainCharacter/PlayerControlerJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlerJump : MonoBehaviour   
+{
+    public Rigidbody rb;
+    private bool isGrounded;
+    private void isJumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isJumping();
+        }
+    }
+}
